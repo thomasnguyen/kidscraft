b68c8107a42a8dd19a6ecc581bb2873eae26d9f1
thomasnguyen <imtoshiru@gmail.com>
2020-06-13 18:02:14 -0700

    Support creating item with images

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -41,6 +41,30 @@ class CreateItem extends Component {
     this.setState({ [name]: val });
   };
 
+  uploadFile = async (e) => {
+    console.log("Uploading  file...");
+    const files = e.target.files;
+    const data = new FormData();
+
+    data.append("file", files[0]);
+    data.append("upload_preset", "sickfits");
+
+    const res = await fetch(
+      "https://api.cloudinary.com/v1_1/dsuvefqs2/image/upload",
+      {
+        method: "POST",
+        body: data,
+      }
+    );
+
+    const file = await res.json();
+    console.log(file);
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url,
+    });
+  };
+
   render() {
     const { title, description, image, largeImage, price } = this.state;
     return (
@@ -62,6 +86,23 @@ class CreateItem extends Component {
           >
             <ErrorMessage error={error} />
             <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="file">
+                Image
+                <input
+                  type="file"
+                  id="file"
+                  name="file"
+                  placeholder="upload an image"
+                  onChange={this.uploadFile}
+                />
+                {this.state.image && (
+                  <img
+                    width="200"
+                    src={this.state.image}
+                    alt="Upload Preview"
+                  />
+                )}
+              </label>
               <label htmlFor="title">
                 Title
                 <input
@@ -94,6 +135,7 @@ class CreateItem extends Component {
                   onChange={this.handleChange}
                 />
               </label>
+              <button type="submit">Submit</button>
             </fieldset>
           </Form>
         )}
