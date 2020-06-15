69bf353121922bf101eb230c2c6d9fdc1a65775e
thomasnguyen <imtoshiru@gmail.com>
2020-06-14 22:26:04 -0700

    👏Feature - pagination functionality

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -5,10 +5,11 @@ import styled from "styled-components";
 
 import Item from "./Item";
 import Pagination from "./Pagination";
+import { perPage } from "../config";
 
 export const ALL_ITEMS_QUERY = gql`
-  query ALL_ITEMS_QUERY {
-    items {
+  query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+    items(first: $first, skip: $skip, orderBy: createdAt_DESC) {
       id
       title
       price
@@ -18,7 +19,6 @@ export const ALL_ITEMS_QUERY = gql`
     }
   }
 `;
-
 const Center = styled.div`
   text-align: center;
 `;
@@ -36,7 +36,13 @@ class Items extends Component {
     return (
       <Center>
         <Pagination page={this.props.page} />
-        <Query query={ALL_ITEMS_QUERY}>
+        <Query
+          query={ALL_ITEMS_QUERY}
+          variables={{
+            skip: this.props.page * perPage - perPage,
+            first: perPage,
+          }}
+        >
           {({ data, loading, error }) => {
             if (loading) return <p>Loading...</p>;
             if (error) return <p>Error: {error.message}</p>;
